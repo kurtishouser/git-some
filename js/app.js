7789fdee96c6c6f1d8515266a730b62ac995ca14
Kurtis Houser <kurtishouser@mac.com>
2017-03-06 20:16:54 -0800

    Add basic Details panel for Repos, Subscriptions and Followers

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
-var baseurl = 'https://api.github.com/users/';
+var baseurl = 'https://api.github.com/';
 var perPage = 12;
 
 function getJSON(url) {
+    console.log('GET', url);
   // Return a new promise.
   return new Promise(function(resolve, reject) {
     // Do the usual XHR stuff
@@ -33,6 +34,23 @@ function getJSON(url) {
   });
 }
 
+function accountDetails(response) {
+    var html = '';
+    html += '<div><strong>Account Type: </strong>' + response.type + '</div>';
+    html += '<div><strong>Name: </strong>' + response.name + '</div>';
+    html += '<div><strong>Company: </strong>' + response.company + '</div>';
+    html += '<div><strong>Location: </strong>' + response.location + '</div>';
+    html += '<div><strong>Email: </strong>' + response.email + '</div>';
+    html += '<div><strong>Bio: </strong>' + response.bio + '</div>';
+    html += '<div><strong>Public Repos: </strong>' + response.public_repos + '</div>';
+    html += '<div><strong>Public Gists: </strong>' + response.public_gists + '</div>';
+    html += '<div><strong>Followers: </strong>' + response.followers + '</div>';
+    html += '<div><strong>Following: </strong>' + response.following + '</div>';
+    html += '<div><strong>Created: </strong>' + response.created_at + '</div>';
+    html += '<div><strong>Updated: </strong>' + response.updated_at + '</div>';
+    return html;
+}
+
 function htmlLinksList(obj, key, text) {
     var html = '<ul class="list-group">';
     obj.forEach(function (item) {
@@ -41,32 +59,29 @@ function htmlLinksList(obj, key, text) {
     html += '</ul>';
     return html;
 }
+
+function htmlGrid(obj, key, text) {
+    var html = '<div class="row">';
+    obj.forEach(function (item) {
+        html += '<div class="col-md-4 item">' + item[text] + '</div>';
+    });
+    html += '</div>';
+    return html;
+}
+
 // git username submitted
 $('.btn.btn-default').click(function(){
-    // console.log('Name submitted', $('.name').val());
     $('#error').text(''); // clear any previous errors
+    $('.panel.panel-info').hide();
     var username = $('.name').val();
-    getJSON(baseurl + username).then(function(response) {
+    getJSON(baseurl + 'users/' + username).then(function(response) {
 
         $('#git_login').show();
         $('#username').hide();
         $('#git_login a').text(response.login);
         $('#git_login a').attr('href', response.html_url);
         $('#git_avatar_url').attr('src', response.avatar_url);
-        var html = '';
-        html += '<div><strong>Account Type: </strong>' + response.type + '</div>';
-        html += '<div><strong>Name: </strong>' + response.name + '</div>';
-        html += '<div><strong>Company: </strong>' + response.company + '</div>';
-        html += '<div><strong>Location: </strong>' + response.location + '</div>';
-        html += '<div><strong>Email: </strong>' + response.email + '</div>';
-        html += '<div><strong>Bio: </strong>' + response.bio + '</div>';
-        html += '<div><strong>Public Repos: </strong>' + response.public_repos + '</div>';
-        html += '<div><strong>Public Gists: </strong>' + response.public_gists + '</div>';
-        html += '<div><strong>Followers: </strong>' + response.followers + '</div>';
-        html += '<div><strong>Following: </strong>' + response.following + '</div>';
-        html += '<div><strong>Created: </strong>' + response.created_at + '</div>';
-        html += '<div><strong>Updated: </strong>' + response.updated_at + '</div>';
-        $('#overview .panel-body').html(html);
+        $('#overview .panel-body').html(accountDetails(response));
         return response;
 
     }, function(error) {
@@ -74,16 +89,40 @@ $('.btn.btn-default').click(function(){
   }).then(function(response) {
 
       getJSON(response.repos_url + '?per_page=' + perPage).then(function(response) {
-          $('#repos .panel-body').html(htmlLinksList(response, 'html_url', 'name'));
+          $('#repos .panel.panel-primary .panel-body').html(htmlGrid(response, 'html_url', 'name'));
+          $('#repos .panel-body .item').click(function() {
+              var item = $(this).text();
+              getJSON(baseurl + 'repos/' + username + '/' + item).then(function(response) {
+                  $('#repos .panel.panel-info').show();
+                  $('#repos .panel.panel-info .panel-title').text('Details for ' + response.name);
+                  $('#repos .panel.panel-info .panel-body').text(response.description);
+              })
+          })
       })
 
       getJSON(response.subscriptions_url + '?per_page=' + perPage).then(function(response) {
-          $('#subscriptions .panel-body').html(htmlLinksList(response, 'html_url', 'full_name'));
+          $('#subscriptions .panel.panel-primary .panel-body').html(htmlGrid(response, 'html_url', 'full_name'));
+          $('#subscriptions .panel-body .item').click(function() {
+              var item = $(this).text();
+              getJSON(baseurl + 'repos/' + item).then(function(response) {
+                  $('#subscriptions .panel.panel-info').show();
+                  $('#subscriptions .panel.panel-info .panel-title').text('Details for ' + response.full_name);
+                  $('#subscriptions .panel.panel-info .panel-body').text(response.description);
+              })
+          })
       })
 
       getJSON(response.followers_url + '?per_page=' + perPage).then(function(response) {
 
-          $('#followers .panel-body').html(htmlLinksList(response, 'html_url', 'login'));
+          $('#followers .panel.panel-primary .panel-body').html(htmlGrid(response, 'html_url', 'login'));
+          $('#followers .panel-body .item').click(function() {
+              var item = $(this).text();
+              getJSON(baseurl + 'users/' + item).then(function(response) {
+                  $('#followers .panel.panel-info').show();
+                  $('#followers .panel.panel-info .panel-title').text('Details for ' + response.login);
+                  $('#followers .panel.panel-info .panel-body').html(accountDetails(response));
+              })
+          })
       })
 
     })
